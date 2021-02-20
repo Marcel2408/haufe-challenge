1e109c967f5af39c750f1d3cac7b2cee6272a303
Marcel Subirana <msubiranac@gmail.com>
2021-02-20 15:41:56 +0100

    feat(global): add initial config

---
+++ client/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: 'babel-eslint',
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:import/errors',
+    'prettier',
+    'prettier/react',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'import', 'jsx-a11y'],
+  rules: {
+    'no-console': 1,
+    'no-use-before-define': 'off',
+    'prettier/prettier': 'warn',
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx'] }],
+    'import/extensions': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+};
