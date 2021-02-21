dd6b371e91d65c10ed647abd890e0ef92aa57542
Marcel Subirana <msubiranac@gmail.com>
2021-02-21 08:44:34 +0100

    refactor(usermodel): add new fields to User Schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,34 @@
+const validator = require('validator');
 const mongoose = require('.');
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name: String,
-  password: String,
-  favourites: [String],
-  timeStamp: {
-    type: Date,
-    default: Date.now,
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
   },
+  email: {
+    type: String,
+    required: [true, 'Please tell us your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please specify your password!'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password!'],
+  },
+  favourites: [String],
+  // timeStamp: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
 });
 
 const User = mongoose.model('User', userSchema);
